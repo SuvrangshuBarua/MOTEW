7ea006275145a7bd3fea291e14662008477ef5de
Martino Pagliarani <martipaglia02@gmail.com>
2025-09-21 15:29:18 +0200

    Refactor Navigation script to improve random destination selection and adjust timing for goal updates

--- Assets/Scripts/Humon/Navigation/Navigation.cs
+++ Assets/Scripts/Humon/Navigation/Navigation.cs
@@ -1,35 +1,49 @@
+using Unity.AI.Navigation;
 using UnityEngine;
 
 using UnityEngine.AI;
-using System.Collections;
 
 public class Navigation : MonoBehaviour
 {
+    public NavMeshAgent Agent { get { return agent; } }
     private NavMeshAgent agent;
+    [SerializeField] private NavMeshSurface surface;
     private float nextGoalTime;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         SetRandomDestination();
-        nextGoalTime = Time.time + Random.Range(2f, 5f);
+        nextGoalTime = Time.time + Random.Range(3f, 8f);
     }
 
     void Update()
     {
+        if (agent.remainingDistance <= 1.0f || !agent.hasPath)
+        {
+            SetRandomDestination();
+            nextGoalTime = Time.time + Random.Range(3f, 8f);
+        }
+
         if (Time.time >= nextGoalTime)
         {
             SetRandomDestination();
-            nextGoalTime = Time.time + Random.Range(2f, 5f);
+            nextGoalTime = Time.time + Random.Range(3f, 8f);
         }
     }
 
     void SetRandomDestination()
     {
-        Vector3 randomDirection = Random.insideUnitSphere * 10f; // Change 10f to your desired range
-        randomDirection += transform.position;
+        Bounds bounds = surface.navMeshData.sourceBounds;
+
+        Vector3 randomPoint = new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            transform.position.y,
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
+
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(randomDirection, out hit, 10f, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(randomPoint, out hit, 10f, 1 << NavMesh.GetAreaFromName("Walkable")))
         {
             agent.SetDestination(hit.position);
         }
