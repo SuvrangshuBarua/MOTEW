0a82d30288a5d35e0a508a8a56b60a02a6082f55
vcler <66980706+vcler@users.noreply.github.com>
2025-09-23 15:48:00 +0200

    added perception system
    * made Draggable compatible with objects that have multiple colliders
    
    * added perception system
    
    * added Perception instance to Humon
    
    * adjust project settings

--- Assets/Scripts/Humon/Humon.cs
+++ Assets/Scripts/Humon/Humon.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Humon : MonoBehaviour
 {
+    private Perception _perception;
+
     void Start()
     {
-        
+        _perception = gameObject.AddComponent<Perception>();
     }
 
     void Update()
