6a36dca5f8906eb84b7d2edf60803c0fad516c58
Lme20 <louisme.2014@gmail.com>
2025-09-25 10:49:08 +0200

    Removed gameplay code from IDamageable interface

--- Assets/Scripts/HealthSystem/IDamageable.cs
+++ Assets/Scripts/HealthSystem/IDamageable.cs
@@ -1,42 +1,10 @@
 using UnityEngine;
 
-public class ClickToDamage : MonoBehaviour
+// Generic interface for damageable objects
+// Should be kept simple, should not handle computation of damage
+// exposes TakeDamage method
+
+public interface IDamageable
 {
-    [SerializeField] private Camera cam;
-    [SerializeField] private int damagePerTap = 10;
-    [SerializeField] private LayerMask damageableMask = ~0;
-
-    private void Reset()
-    {
-        if (!cam) cam = Camera.main;
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            DoDamageAt(Input.mousePosition, "mouse");
-            Debug.Log("DoDamageAt called"); // idk
-        }
-
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            DoDamageAt(Input.GetTouch(0).position, "touch");
-            Debug.Log("DoDamageAt called");
-        }
-    }
-
-    private void DoDamageAt(Vector2 screenPos, object source)
-    {
-        if (!cam) return;
-        Ray ray = cam.ScreenPointToRay(screenPos);
-        if (Physics.Raycast(ray, out var hit, Mathf.Infinity, damageableMask))
-        {
-            var damageable = hit.collider.GetComponentInParent<IDamageable>();
-            if (damageable != null)
-            {
-                damageable.TakeDamage(damagePerTap, source);
-            }
-        }
-    }
+    void TakeDamage(int amount, object source = null);
 }
