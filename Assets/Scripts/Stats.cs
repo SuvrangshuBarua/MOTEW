5c3ea12e8f970edd07aa28643df9c86e7d408134
Lme20 <louisme.2014@gmail.com>
2025-09-25 12:23:33 +0200

    Add fall damage stats in Stats.cs

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Stats", menuName = "MOTEW/Stats", order = 51)]
 public class Stats : ScriptableObject
 {
+    [Header("Base Stats")]
     [SerializeField] private int _baseHealth = 100;
     [SerializeField] private float _baseRange = 1;
     [SerializeField] private float _baseSpeed = 2;
-    
+
     public int BaseHealth => _baseHealth;
     public float BaseRange => _baseRange;
     public float BaseSpeed => _baseSpeed;
+
+    [Header("Fall Damage")]
+    [SerializeField] private float _minImpactSpeed = 7.5f;
+    [SerializeField] private float _damagePerSpeed = 2.0f;
+    [SerializeField] private int _maxFallDamage = 50;
+
+    public float MinImpactSpeed => _minImpactSpeed;
+    public float DamagePerSpeed => _damagePerSpeed;
+    public int MaxFallDamage => _maxFallDamage;
 }
