91cb47d371dc5aab91952187260857b750d009ca
SuvrangshuBarua <suvrangshu.turno@gmail.com>
2025-09-25 14:43:45 +0200

    scraping BT

--- Assets/Scripts/Humon/Humon.cs
+++ Assets/Scripts/Humon/Humon.cs
@@ -1,17 +1,66 @@
+using System;
+using God;
+using StateMachine;
+using Unity.Behavior;
 using UnityEngine;
 
 public class Humon : MonoBehaviour
 {
     private Perception _perception;
+    public Perception Perception => _perception;
+    
+    private Navigation _navigation;
+    public Navigation Navigation => _navigation;
+    
+    private BehaviorGraphAgent _agent;
+    private StateMachine.StateMachine _stateMachine;
+    private Draggable _draggable;
+
+    private void Awake()
+    {
+        _perception = gameObject.AddComponent<Perception>();
+        _navigation = gameObject.GetComponent<Navigation>();  
+        _agent = gameObject.GetComponent<BehaviorGraphAgent>();
+        _draggable = gameObject.GetComponent<Draggable>();
+        
+        _stateMachine = new StateMachine.StateMachine(this);
+    }
 
     void Start()
     {
-        _perception = gameObject.AddComponent<Perception>();
+        
+        InitializeStateMachine();
+        _draggable.OnStartDrag += OnStartDrag;
+        _draggable.OnDragEnd += OnDragEnd;
     }
 
-    void Update()
+    private void InitializeStateMachine()
     {
+        _stateMachine.AddState(new RoamState());
         
+        _stateMachine.ChangeState<RoamState>();
     }
+
+    private void Update()
+    {
+        _stateMachine.Update();
+    }
+
+    private void OnDragEnd()
+    {
+        //_agent.BlackboardReference.SetVariableValue("interrupt", false);
+    }
+
+    private void OnStartDrag()
+    {
+        //_agent.BlackboardReference.SetVariableValue("interrupt", true);
+    }
+
+    private void OnDestroy()
+    {
+        _draggable.OnStartDrag -= OnStartDrag;
+        _draggable.OnDragEnd -= OnDragEnd;
+    }
+    
 }
 
--- Assets/Scripts/Humon/Navigation/Navigation.cs
+++ Assets/Scripts/Humon/Navigation/Navigation.cs
@@ -1,31 +1,47 @@
+using System;
 using Unity.AI.Navigation;
 using UnityEngine;
 
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class Navigation : MonoBehaviour
 {
-    public NavMeshAgent Agent { get { return agent; } }
+    
     private NavMeshAgent agent;
+    
     [SerializeField] private NavMeshSurface surface;
     private float nextGoalTime;
+    
+    public NavMeshAgent Agent => agent;
+    public bool IsMoving => agent.velocity.sqrMagnitude > 0.01f;
+    public bool HasPath => agent.hasPath;
+    public float RemainingDistance => agent.remainingDistance;
+    public float Speed => agent.speed;
+    public float RemainingTime => agent.remainingDistance / agent.speed;
+    public bool ReachedDestination => !agent.pathPending && agent.remainingDistance <= 1.0f;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        GetRandomDestination();
-        nextGoalTime = Time.time + Random.Range(3f, 8f);
+        
+        /*GetRandomDestination();
+        nextGoalTime = Time.time + Random.Range(3f, 8f);*/
     }
 
     void Update()
     {
-        if (agent.remainingDistance <= 1.0f || !agent.hasPath || Time.time >= nextGoalTime)
+        /*if (agent.remainingDistance <= 1.0f || !agent.hasPath || Time.time >= nextGoalTime)
         {
             GetRandomDestination();
             nextGoalTime = Time.time + Random.Range(3f, 8f);
-        }
+        }*/
     }
-
+    
     public Vector3 GetRandomDestination()
     {
         Vector3 randomPoint = TakeRandomPoint();
@@ -46,6 +62,12 @@ public class Navigation : MonoBehaviour
             transform.position.y,
             Random.Range(bounds.min.z, bounds.max.z)
         );
+    }
+
+    public void StopMoving()
+    {
+        if(agent.isOnNavMesh)
+             agent.ResetPath();
     }
 
     public void SetDestination(Vector3 position)
