0e94fdc085575c8d1c5de13c5ff36eefed00d0c7
SuvrangshuBarua <suvrangshu.turno@gmail.com>
2025-09-26 14:33:35 +0200

    the navigation now works perfectly

--- Assets/Scripts/StateMachine/States.cs
+++ Assets/Scripts/StateMachine/States.cs
@@ -59,19 +59,28 @@ namespace StateMachine
     
     public class InAirState : IState
     {
+        private bool m_dropInitiated = false;
         public void Enter(Humon npc)
         {
-            
+            m_dropInitiated = false;
+            if (npc.Navigation.Agent.enabled)
+            {
+                npc.Navigation.Agent.enabled = false;
+            }
         }
 
         public void Update(Humon npc)
         {
-            
+            if (!npc.IsBeingDragged && !m_dropInitiated)
+            {
+                m_dropInitiated = true;
+                npc.DropToSurface();
+            }
         }
 
         public void Exit(Humon npc)
         {
-            
+            m_dropInitiated = false;
         }
     }
 }
